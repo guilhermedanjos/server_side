a00d64bf6eff05b21379e68d6b8cbda6464087a5
levinomoises <levino_moises@hotmail.com>
2015-10-23 09:49:07 -0200

    Update server Handler to call the parse function
    Signed-off-by: levinomoises <levino_moises@hotmail.com>

--- configuration_files/parser.py
+++ configuration_files/parser.py
@@ -1,6 +1,10 @@
 import json
 
-with open("instruction.json") as instructions_file:
-    instructions_data = json.load(instructions_file)
-    #print(instructions_data)
-    print(instructions_data['moveForward'])
+class Parser():
+
+  def parseJson(Jsonfile):
+    with open("instruction.json") as instructions_file:
+      instructions_data = json.load(instructions_file)
+      print(Jsonfile)
+      print(instructions_data)
+    return 
--- configuration_files/server_rapps.py
+++ configuration_files/server_rapps.py
@@ -1,19 +1,19 @@
 import SocketServer
+from parser.py import Parser
 
 class AlfaTCPHandler(SocketServer.BaseRequestHandler):
-  """
-  This class are responsable to handle connection
-  Instantiated once per connection to the server
-  Override the handle() method to implement communication
-  """
+  # Get the parser
+  parser = Parser()
+
+  # Handle the AndroidApp request
   def handle(self):
     self.data = self.request.recv(1024).strip()
-    print "{} wrote:".format(self.client_address[0])
-    print self.data
+    # Parse the Json file
+    parser.parseJson(data)
 
+# Stablish connections with the AndroidApp
 if __name__ == "__main__":
-  HOST, PORT = "10.42.0.1", 7394
-
+  # TODO define HOST/PORT
+  HOST, PORT = "10.0.0.1", 7394
   server = SocketServer.TCPServer((HOST,PORT),AlfaTCPHandler)
   server.serve_forever()
-
